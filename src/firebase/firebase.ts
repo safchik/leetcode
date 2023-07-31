96271eab30916025c74c507ea81be04cfed075c6
safchik <safchik9@gmail.com>
2023-07-31 14:29:16 +0100

    firebase setup completed

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBW7a_8jJT6WUkltyN-OvBvn1Co-0hpQlQ",
-    authDomain: "leetclone-95bfc.firebaseapp.com",
-    projectId: "leetclone-95bfc",
-    storageBucket: "leetclone-95bfc.appspot.com",
-    messagingSenderId: "701630245006",
-    appId: "1:701630245006:web:1ee4af18a5c0357cc181df"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
